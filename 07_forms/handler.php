c04a1ea52bb4b3f2df3d2a6d3772399125ebe836
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2025-03-03 00:50:09 +0300

    add: forms

---
+++ 07_forms/handler.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once 'dd.php';
+
+// dd($_GET);
+
+dd($_POST);
+
+// dd(file_get_contents('php://input'));
+
+// echo file_get_contents('php://input');
+
+
+
--- http_file_server/FileServer/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿
-using FileServer;
-
-Server server = new Server(8080)
-{
-    RootDirectory = @"C:\itstep\p23\p23_html_css",
-};
-
-await server.StartAsync();
